2c22eb756928e9966ace75221b419b2db7cb7c92
Diego24Vas <dvasquez2024@alu.uct.cl>
2024-10-16 17:43:34 -0300

    se agrego funcion suma menu en js

--- Entrega_2/js/funciones.js
+++ Entrega_2/js/funciones.js
@@ -1,4 +1,29 @@
 
+// FUNCION QUE SUMA LA CANTIDAD DE PRODUCTOS Y CALCULA EL TOTAL
+let total = 0;
+function modificarCantidad(precio, idCantidad, cantidad) {
+    let elementoCantidad = document.getElementById(idCantidad);
+    let cantidadActual = parseInt(elementoCantidad.textContent);
+
+    // Actualizamos la cantidad, y impide menos que cero
+    let nuevaCantidad = cantidadActual + cantidad;
+    if (nuevaCantidad < 0) return;
+
+    // Actualiza la cantidad
+    elementoCantidad.textContent = nuevaCantidad;
+
+    // Calculamos el total
+    total += precio * cantidad;
+    document.getElementById('total').textContent = `$${total.toLocaleString()}`;
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    alert("Pedido realizado con éxito");
+}
+
+
+
 // FUNCION PARA MOSTRAR DATOS INGRESADOS EN FORMULARIO
 function handleSubmit(event) {
     event.preventDefault();                                    // Previene el envío real del formulario
@@ -17,8 +42,6 @@ document.querySelector('header').addEventListener('mouseover', () => {
     // Muestra una alerta cuando el mouse pasa sobre el header
     alert('Bienvenido a SUSHITO SHAN-GAI, el mejor sushi en Temuco!');
 });
-
-
 
 
 
