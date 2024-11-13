ccf5efa1525acf9534b2395f4e4ee8fbe3b9767e
Diego24Vas <dvasquez2024@alu.uct.cl>
2024-11-13 19:35:28 -0300

    problema mysql solucionado

--- Entrega_3/php/conex.php
+++ Entrega_3/php/conex.php
@@ -4,15 +4,13 @@ $username = "rpedraza";           // Nombre de usuario de la base de datos
 $password = "jV25+CCMgXZaG9360";  // Contraseña de la base de datos
 $dbname = "A2024_rpedraza";       // Nombre de la base de datos
 
-try {
-    // Crear conexión usando PDO
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    
-    // Establecer el modo de error de PDO a excepción
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-} catch (PDOException $e) {
-    // Si hay un error en la conexión, mostrar mensaje
-    die(json_encode(["error" => "Conexión fallida: " . $e->getMessage()]));
+// Crear conexión usando mysqli_connect
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if (!$conn) {
+    die(json_encode(["error" => "Conexión fallida: " . mysqli_connect_error()]));
 }
+
 ?>
+
